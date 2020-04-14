f28989ce792aa394dd450555ff1e75d4ec7a75bb
yuuchikura <yu.major@gmail.com>
2020-04-14 12:58:58 +0900

    non-repeat random number generator

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ import random
 app.register_blueprint(setBlueprint.js)
 app.register_blueprint(setBlueprint.css)
 app.register_blueprint(setBlueprint.image)
-randomize_cards_cache=[]
+randomize_cards_cache = []
 
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
@@ -23,5 +23,14 @@ def index():
 @app.route('/randomcards', methods=['GET'])
 def randomize():
     global randomize_cards_cache
-    randomNum = random.randint(1,13)
-    return str(randomNum)
+    n = 13
+    l = len(randomize_cards_cache)
+    while l < n:
+        r = random.randint(1,n)
+        if r not in randomize_cards_cache:
+            randomize_cards_cache.append(r)
+            return str(r)
+    else:
+        randomize_cards_cache.clear()
+        randomize_cards_cache.append(1)
+        return str(1)
