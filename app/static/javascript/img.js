450a94fbe59ec0c14402cf168d7c8f7f0314c32c
yuuchikura <yu-uchikura@g.ecc.u-tokyo.ac.jp>
2020-05-08 21:53:25 +0900

    update
    - shuffle alart
    - image sprites

--- app/static/javascript/img.js
+++ app/static/javascript/img.js
@@ -20,59 +20,59 @@ function randomize_image_character_cards() {
 
 
 
-/* random generater : context_cards */
-function randomize_image_context_cards() {
-    $.ajax({
-        type: 'GET',
-        url: "/randomNum_context_cards",
-        contentType: 'application/json;charset=UTF-8',
-        success: function(result) {
-            var res = result.split(' ')
-            var imgName = "context_card-" + res[0] + ".jpg";
-            document.getElementById("context_cards_imageid").src= "../../static/image/context_cards" + "/" + imgName;
-            document.getElementById("count").innerHTML= res[1];  
-        },
-        error: function(result) {
-              document.getElementById("cards").innerHTML = "ERROR!";
-        }
-    });
-};
+// /* random generater : context_cards */
+// function randomize_image_context_cards() {
+//     $.ajax({
+//         type: 'GET',
+//         url: "/randomNum_context_cards",
+//         contentType: 'application/json;charset=UTF-8',
+//         success: function(result) {
+//             var res = result.split(' ')
+//             var imgName = "context_card-" + res[0] + ".jpg";
+//             document.getElementById("context_cards_imageid").src= "../../static/image/context_cards" + "/" + imgName;
+//             document.getElementById("count").innerHTML= res[1];  
+//         },
+//         error: function(result) {
+//               document.getElementById("cards").innerHTML = "ERROR!";
+//         }
+//     });
+// };
 
-/* random generater : parameter_cards */
-function randomize_image_parameter_cards() {
-    $.ajax({
-        type: 'GET',
-        url: "/randomNum_parameter_cards",
-        contentType: 'application/json;charset=UTF-8',
-        success: function(result) {
-            var res = result.split(' ')
-            var imgName = "parameter_card-" + res[0] + ".jpg";
-            document.getElementById("parameter_cards_imageid").src= "../../static/image/parameter_cards" + "/" + imgName;
-            document.getElementById("count").innerHTML= res[1];  
-        },
-        error: function(result) {
-              document.getElementById("cards").innerHTML = "ERROR!";
-        }
-    });
-};
+// /* random generater : parameter_cards */
+// function randomize_image_parameter_cards() {
+//     $.ajax({
+//         type: 'GET',
+//         url: "/randomNum_parameter_cards",
+//         contentType: 'application/json;charset=UTF-8',
+//         success: function(result) {
+//             var res = result.split(' ')
+//             var imgName = "parameter_card-" + res[0] + ".jpg";
+//             document.getElementById("parameter_cards_imageid").src= "../../static/image/parameter_cards" + "/" + imgName;
+//             document.getElementById("count").innerHTML= res[1];  
+//         },
+//         error: function(result) {
+//               document.getElementById("cards").innerHTML = "ERROR!";
+//         }
+//     });
+// };
 
-/* random generater : rammojammo_cards */
-function randomize_image_rammojammo_cards() {
-    $.ajax({
-        type: 'GET',
-        url: "/randomNum_rammojammo_cards",
-        contentType: 'application/json;charset=UTF-8',
-        success: function(result) {
-            var res = result.split(' ')
-            var imgName = "rammojammo_card-" + res[0] + ".jpg";
-            document.getElementById("rammojammo_cards_imageid").src= "../../static/image/rammojammo_cards" + "/" + imgName;
-            document.getElementById("count").innerHTML= res[1];
-        },
-        error: function(result) {
-              document.getElementById("cards").innerHTML = "ERROR!";
-        }
-    });
-};
+// /* random generater : rammojammo_cards */
+// function randomize_image_rammojammo_cards() {
+//     $.ajax({
+//         type: 'GET',
+//         url: "/randomNum_rammojammo_cards",
+//         contentType: 'application/json;charset=UTF-8',
+//         success: function(result) {
+//             var res = result.split(' ')
+//             var imgName = "rammojammo_card-" + res[0] + ".jpg";
+//             document.getElementById("rammojammo_cards_imageid").src= "../../static/image/rammojammo_cards" + "/" + imgName;
+//             document.getElementById("count").innerHTML= res[1];
+//         },
+//         error: function(result) {
+//               document.getElementById("cards").innerHTML = "ERROR!";
+//         }
+//     });
+// };
 
 
 /* humberger menu */
